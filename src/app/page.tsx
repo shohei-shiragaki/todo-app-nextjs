dc0b00ae1a227b5de043c5ae69ac4911164819c2
shohei-shiragaki <126951181+shohei-shiragaki@users.noreply.github.com>
2025-01-18 21:04:22 +0900

    削除機能2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { getAllTodos } from "@/todoAPI";
 import TodoList from "./component/TodoList";
-import Link from 'next/link';
 
 export default async function Home() {
   const todoList = await getAllTodos()
