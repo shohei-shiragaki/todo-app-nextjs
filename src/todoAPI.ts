68d4d82601fd0c2955c036b19af793d67860c496
shohei-shiragaki <126951181+shohei-shiragaki@users.noreply.github.com>
2025-02-14 20:59:02 +0900

    バリデーション2

--- src/todoAPI.ts
+++ src/todoAPI.ts
@@ -3,8 +3,8 @@ import { Todo, TodoCreate } from "./types";
 // 全TODOを取得
 export const getAllTodos = async (): Promise<Todo[]> => {
 
-    // const res = await fetch('https://todo-api-aa9t.onrender.com', { cache: 'no-store' });
-    const res = await fetch('http://127.0.0.1:8000', { cache: 'no-store' });
+    const res = await fetch('https://todo-api-aa9t.onrender.com', { cache: 'no-store' });
+    // const res = await fetch('http://127.0.0.1:8000', { cache: 'no-store' });
     const todos:Todo[] = await res.json();
     return todos;
 
@@ -13,8 +13,8 @@ export const getAllTodos = async (): Promise<Todo[]> => {
 // IDからTODOを取得
 export const getTodoById = async (id: string): Promise<Todo> => {
 
-  // const res = await fetch(`https://todo-api-aa9t.onrender.com/todos/${id}`, { cache: 'no-store' });
-  const res = await fetch(`http://127.0.0.1:8000/todos/${id}`, { cache: 'no-store' });
+  const res = await fetch(`https://todo-api-aa9t.onrender.com/todos/${id}`, { cache: 'no-store' });
+  // const res = await fetch(`http://127.0.0.1:8000/todos/${id}`, { cache: 'no-store' });
   const todo:Todo = await res.json();
   return todo;
 
@@ -23,8 +23,8 @@ export const getTodoById = async (id: string): Promise<Todo> => {
 // TODOリストを削除
 export const deleteTodoList = async (selectTodos: Todo[]) : Promise<Response> => {
 
-    // const res = await fetch('https://todo-api-aa9t.onrender.com/todo-delete', {
-    const res = await fetch('http://127.0.0.1:8000/todo-delete', {
+    const res = await fetch('https://todo-api-aa9t.onrender.com/todo-delete', {
+    // const res = await fetch('http://127.0.0.1:8000/todo-delete', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -38,8 +38,8 @@ export const deleteTodoList = async (selectTodos: Todo[]) : Promise<Response> =>
 // TODOリストを更新
 export const updateTodo = async (id:string, req:Todo) : Promise<Response> => {
 
-    // const res = await fetch(`https://todo-api-aa9t.onrender.com/todos/${id}`, {
-    const res = await fetch(`http://127.0.0.1:8000/todos/${id}`, {
+    const res = await fetch(`https://todo-api-aa9t.onrender.com/todos/${id}`, {
+    // const res = await fetch(`http://127.0.0.1:8000/todos/${id}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
@@ -52,8 +52,8 @@ export const updateTodo = async (id:string, req:Todo) : Promise<Response> => {
 // TODOを作成
 export const createTodo = async (req:TodoCreate) : Promise<Response> => {
 
-  // const res = await fetch('https://todo-api-aa9t.onrender.com/todo-create', {
-  const res = await fetch('http://127.0.0.1:8000/todo-create', {
+  const res = await fetch('https://todo-api-aa9t.onrender.com/todo-create', {
+  // const res = await fetch('http://127.0.0.1:8000/todo-create', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
