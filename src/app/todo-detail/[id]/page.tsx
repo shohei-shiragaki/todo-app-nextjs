51d7629414ce44da6b75ec03199a4502b0bd5786
shohei-shiragaki <126951181+shohei-shiragaki@users.noreply.github.com>
2025-03-19 07:40:50 +0900

    待機時間

--- src/app/todo-detail/[id]/page.tsx
+++ src/app/todo-detail/[id]/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import DetailPage from "@/app/component/DetailPage";
 import { getTodoById } from "@/todoAPI";
+import Loading from "@/app/Loading";
 
 const page = async({ params }: { params: Promise<{ id: string }> }) => {
 
     const { id } = await params;
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
     const todoData = await getTodoById(id);
-
+    if (!todoData) {
+        return delay(5000).then(() => <Loading />);
+    }
     return (
         <DetailPage todo={todoData}/>
     )
