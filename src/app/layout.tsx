b3458e941b627430e08ba2add71d16590f0b6651
shohei-shiragaki <126951181+shohei-shiragaki@users.noreply.github.com>
2025-04-19 23:10:19 +0900

    不要なコード削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Header } from "@/component/header";
 import { Footer } from "@/component/footer";
-import "./globals.css";
+import "@/app/globals.css";
 import type { Metadata } from "next";
 import { Suspense } from "react";
 import Loading from "@/component/loading";
@@ -8,7 +8,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './globals.css';
 
 export const metadata: Metadata = {
   title: "ToDo App",
